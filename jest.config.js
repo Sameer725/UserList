59b7d780b8c164a0557a7112427a7d7cb4c6965f
Sam Eer <samkhanal725@gmail.com>
2019-11-13 12:44:26 +0545

    Test Implementation for View Component

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: "ts-jest",
+  verbose: true,
+  bail: 5,
+  testEnvironment: "jsdom",
+  roots: ["<rootDir>/src"],
+  transform: {
+    "\\.(ts|tsx)$": "ts-jest"
+  },
+  testPathIgnorePatterns: ["/node_modules/"],
+  moduleFileExtensions: ["ts", "tsx", "js", "json"],
+  setupFilesAfterEnv: ["<rootDir>/setup/setupTests.ts"]
+};
